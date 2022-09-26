cc12b916b633198b6ff3da119cfb3414e4a91ac4
clarasaffre <86710075+clarasaffre@users.noreply.github.com>
2022-09-26 16:05:47 +0200

    INR

---
+++ INR_clara/articles.php
@@ -0,0 +1,185 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+
+
+<section>
+      
+  <aside>    
+    <h2>
+      Clara Saffre 
+      <a href="https://clarasaffre.github.io/accueil.html" target="_blank">portfolio</a>
+    </h2>
+    <p>
+      J'essaye de comprendre quels sont les limites et relations entre graphisme et art
+      en questionnant à quel moment le graphisme devient de l'art contemporain&#8239? <br>
+      qu'est-ce que produit le déplacement d'un objet graphique dans un lieu d'exposition&#8239?
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv art tag2">
+    <h3>
+      BOUIGE, Caroline.  
+    </h3>
+
+    <h4>
+      <a href="https://fr.shopping.rakuten.com/offer/buy/3043780638/etapes-numero-245-format-broche.html" target="_blank">
+        Design graphique et Art contemporain friction, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Étape&#8239: édition, Sept - Oct 2018.
+    </h5>
+    
+    <blockquote cit="#">Des graphistes s'emparent de l'œuvre d'art et des artistes utilisent dans leur travails des éléments appartenant à la culture propre du graphisme ou y faisant référence</blockquote>
+    
+    <p> 
+      Lionel Bovier, commissaire d'exposition indépendant du MAMCO de Genève parle du choix d'intégrer une direction artistique au livre d'art. Comment restituer une exposition dans une édition qui fait sens&#8239? Comment le designer s'empare du rôle d'auteur&#8239?
+    </p>
+   
+
+    <h6> 
+      lu le 13 septembre 2022  
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv art designers">
+    <h3>
+      BOUIGE, Caroline.  
+    </h3>
+
+    <h4>
+      <a href="https://fr.shopping.rakuten.com/offer/buy/3043780638/etapes-numero-245-format-broche.html" target="_blank">
+        L'art est une rencontre, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Étape&#8239: édition, n°245, Sept - Oct 2018.
+    </h5>
+    
+    <blockquote cit="#">Il y a un moment où nous nous sommes mis à travailler l'exposition en tant que format et non comme des designers dessinent une signalétique dans une exposition d'art contemporain</blockquote>
+    
+    <p> 
+      Dans une interview entre Caroline Bouige et l'un des membre de M/M, Mathias Augustyniak, le graphiste revient sur des projets phares réalisés pour des expositions d'art contemporain. Le studio M/M a cette position particulière dans le champ du design graphique et de l'art contemporain. Ils se placent toujours dans une position d'auteur questionnant la relation entre «&#8239l'œuvre, l'espace et son public&#8239». Comment travailler l'exposition en tant que format et non comme des designers dessinent une signalétique dans une exposition d'art contemporain&#8239? Comment introduire ses productions graphiques dans une autre expositions, en harmonie&#8239? Comment recontextualiser des productions graphiques dans une exposition&#8239? 
+    </p>
+   
+
+    <h6> 
+      lu le 13 septembre 2022  
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv art designers">
+    <h3>
+      AUCOMPTE, Yann.  
+    </h3>
+
+    <h4>
+      <a href="http://www.cairn.info/revue-marges-2019-2-page-30.htm" target="_blank">
+        Le graphisme sur le terrain de l’art contemporain&#8239: le cas de M/M (&#8239Paris&#8239), 
+      </a>
+    </h4> 
+    
+    <h5>
+      Revue Marges, 2019.
+    </h5>
+    
+    <blockquote cit="http://www.cairn.info/revue-marges-2019-2-page-30.htm">Nous voulions débarrasser le graphisme de son image de prestataire de service destiné à favoriser la médiation de l’art dans les réseaux de communication</blockquote>
+    
+    <p> 
+      Dans cette étude de cas, Yann Aucompte tente de comprendre comment la pratique graphique du studio M/M tend vers l'art contemporain. Le studio M/M possède un statut particulier appelé «&#8239graphiste-auteur&#8239» qui tente de se libérer des contraintes du «&#8239graphisme [à l'] image de prestataire de service destiné à favoriser la médiation de l’art dans les réseaux de communication&#8239» en s'émancipant du rôle fonctionnaliste de la communication (&#8239invitant le lecteur à décrypter&#8239), en utilisant des techniques analogues à l'art (&#8239peinture, photographie…&#8239)Mais le duo ne se serait-il juste pas emparé de la communication «&#8239destinée au grand public&#8239» pour véhiculer leur expression artistique&#8239? Finalement, M/M ne se serait-il pas fait "enrôlé" par une autre institution&#8239? 
+    </p>
+   
+
+    <h6> 
+      consulté le 14 septembre 2022  
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv art designers">
+    <h3>
+      LYOTARD, Jean-François.  
+    </h3>
+
+    <h4>
+      <a href="https://blogs.esam-c2.fr/design-graphique/intriguer-ou-le-paradoxe-du-graphiste-de-jean-francois-lyotard/" target="_blank">
+        Intriguer, ou le paradoxe du graphiste, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Blogs esam, 2019.
+    </h5>
+    
+    <blockquote cit="https://blogs.esam-c2.fr/design-graphique/intriguer-ou-le-paradoxe-du-graphiste-de-jean-francois-lyotard/">En visant le plaisir, l'obiet appartient à l'esthétique. En visant la croyance, il relève de la rhétorique</blockquote>
+    
+    <p> 
+      D'après Jean-François Lyotard, Le graphisme est un «&#8239art de rue&#8239», un «&#8239art éphémère&#8239», conçu pour des évènements compris dans un temps limité. Le graphiste est concepteur d'«&#8239art rapide&#8239» et ce doit d'intriguer le passant pour l'informer. Il doit donc se démarquer esthétiquement tout en respectant l'objet du commanditaire pour qu'on puisse s'arrêter pour le regarder. Cependant, il n'est pas destiné à être exposé, selon l'auteur, puisque d'une part, il ne saura pas satisfaire la sensibilité du nouveau spectateur, «&#8239cette bête obscure&#8239» et perdra tout sens placé hors de son contexte temporel et géographique. Coincé entre le rôle d'artiste et d'interprête, comment le designer parvient-il à s'émanciper&#8239? 
+    </p>
+   
+
+    <h6> 
+      consulté le 22 septembre 2022  
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv edition exposition">
+    <h3>
+      DUPEYRAT, Jérôme.  
+    </h3>
+
+    <h4>
+      <a href="https://revue-faire.eu/produit/n11-a-printed-exhibition-vol-19-by-klaus-scherubel-title-of-the-show-by-julia-born-and-thereherethenthere-by-simon-starling-author-jerome-dupeyrat/" target="_blank">
+        «&#8239Vol 13&#8239» by Klau Scherübel
+        «&#8239Title of the Show&#8239» by Julia Born 
+        and «&#8239THEREHERETHENTHERE&#8239» by Simon Starling,       
+      </a>
+    </h4> 
+    
+    <h5>
+      Revue Faire, n°11, 25 avril 2018.
+    </h5>
+    
+    <blockquote cit="https://revue-faire.eu/produit/n11-a-printed-exhibition-vol-19-by-klaus-scherubel-title-of-the-show-by-julia-born-and-thereherethenthere-by-simon-starling-author-jerome-dupeyrat/">Comment ce nouveau modèle structurel pourrait mettre à plat les hiérarchies établies entre une œuvre, une exposition et une édition et permettre une autre réflexion sur ces catégories, leurs espaces de monstration respectifs ainsi que les rapports qui existent entre eux&#8239?</blockquote>
+    
+    <p> 
+      Dans ce numéro de la revue Faire, il y est présenté 3 cas de figures différents sur le livre-galerie à travers 3 expositions. Comment lier travaux, édition et espace? Je retiens surtout l'exposition «&#8239Title of the Show&#8239» de Julia Born à la Galerie für Zeitgenössische Kunst de Leipzig. Ici, la designeuse pense sa monographie comme un catalogue d'exposition à l'échelle d'une exposition. Sur chaque mur y est présenté une page de ce livre géant. Numérotés, ils guident le spectateur dans l'espace et permet son adaptation imprimé. Il n'y a aucun travail de mise en page dans l'édition imprimé si ce n'est que l'utilisation des photos de l'exposition qui mettent déjà en forme le travaille de Julia Born.
+    </p>
+   
+
+    <h6> 
+      lu le 25 septembre 2022  
+    </h6>
+  
+  </article>
+
+
+</section>
+
+
+
+
